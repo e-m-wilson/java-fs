160a7bb0af1263b9c6e9a336ba95360619447282
Ethan Wilson <ethanwilson1991@gmail.com>
2026-09-01 13:09:09 -0500

    update

--- MyFirstJavaProject/Hello.java
+++ MyFirstJavaProject/Hello.java
@@ -1,27 +1,15 @@
 public class Hello {
+    
+    // single line comment 
 
-    public static void main(String[] args){
+    /* This is a multi-line comment */
+    public static void main(String[] args) {
 
-      
+        boolean b = true;
+        int age = 30;
 
-        class Person {
-            String name;
-        }
-        Person a = new Person();
-        a.name = "Alice";
 
-        Person b = a;
-
-        System.out.println(a == b);
-
-        Person c = new Person(); 
-        Person d = new Person(); 
-        System.out.println(c == d);
-        System.out.println(c.equals(d)); 
-
-        double price = 19.98765;
-
-        System.out.printf("Price: $%.2f%n", price);
-
+        System.out.println(age + 5);
     }
+
 }
