3745db0070ad1e5a0eb1bd0a98dcfcf280c0be70
Ethan Wilson <ethanwilson1991@gmail.com>
2026-09-02 13:02:34 -0500

    update

---
+++ MyFirstJavaProject/com/myapp/Calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.myapp.Calculator;
+
+public class Calculator {
+
+    public static String message = "This is my caclulator!";
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ MyFirstJavaProject/com/myapp/Main.java
@@ -0,0 +1,24 @@
+package com.myapp;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.print("Enter your height: ");
+        double height = sc.nextDouble();
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+
+        sc.close();
+    }
+}
