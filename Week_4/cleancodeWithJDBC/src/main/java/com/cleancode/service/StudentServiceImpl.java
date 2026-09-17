8457a694d49f33f716a4a26a4dee47aa43077e70
Ethan Wilson <ethanwilson1991@gmail.com>
2026-09-17 11:37:52 -0500

    update

--- Week_4/cleancodeWithJDBC/src/main/java/com/cleancode/service/StudentServiceImpl.java
+++ Week_4/cleancodeWithJDBC/src/main/java/com/cleancode/service/StudentServiceImpl.java
@@ -3,17 +3,22 @@ package com.cleancode.service;
 import com.cleancode.domain.Student;
 import com.cleancode.persistence.StudentDAO;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class StudentServiceImpl implements StudentService {
     private final StudentDAO studentDAO;
+    private static Logger logger;
 
     public StudentServiceImpl(StudentDAO studentDAO) {
         this.studentDAO = studentDAO;
+        logger = LoggerFactory.getLogger(StudentServiceImpl.class);
     }
 
     @Override
     public void addStudent(Student student) {
         if (studentDAO.getStudentById(student.getId()) != null) {
+            logger.warn("Student ID: {} already exists!", student.getId());
             throw new IllegalArgumentException("Student ID already exists");
         }
 
@@ -22,6 +27,7 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public Student findStudent(int id) {
+        logger.info("Finding student with ID:{}", id);
         return studentDAO.getStudentById(id);
     }
 
